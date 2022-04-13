ac7383b447657cb31d89b1465bf840600762feb9
user <caid@bxscience.edu>
2022-04-12 21:41:24 -0400

    Finished all Functionality and did some minor touchups

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,46 +1,99 @@
 import { Component } from "react";
-import building from "../buildings.jpeg";
+import React from "react";
+
+var debiturl;
+var crediturl;
+var debitresponse;
+var debitdata;
+var creditresponse;
+var creditdata;
+
+function clearTable(){
+  let table = document.getElementById('myTable');
+  table.innerHTML="";
+  //alert("cleared");
+}
+
+function buildTable(data){
+  var table = document.getElementById('myTable');
+  //clears out old items.
+  clearTable();
+  for (var i = 0; i < data.length; i++){
+    var row = `<tr>
+          <td>${data[i].date.substr(0, 10)}</td>
+          <td>${data[i].description}</td>
+          <td>${data[i].amount}</td>
+          <td>${data[i].id}</td>
+        </tr>`
+      table.innerHTML += row;
+  }
+}
+
+    //should sort them by newest transactions to oldest
+    function custom_sort(a, b) {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    };
+
+function creditordebit(){
+  var input=document.getElementById('cardselect').value;
+
+  if(input=="credit"){
+    buildTable(creditdata.sort(custom_sort));
+  }
+  if(input=="debit"){
+    buildTable(debitdata.sort(custom_sort));
+  }
+  
+  //alert(document.getElementById('cardselect').value);
+
+
+}
 
 class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {numImages: 1};
+
+  state = {
+    loading: true
   }
+  async componentDidMount() {
+    debiturl = "https://moj-api.herokuapp.com/debits";
+    crediturl = "https://moj-api.herokuapp.com/credits";
+    debitresponse = await fetch(debiturl);
+    debitdata = await debitresponse.json();
+    creditresponse = await fetch(crediturl);
+    creditdata = await creditresponse.json();
+    console.log(debitdata.sort(custom_sort));
+    console.log(creditdata.sort(custom_sort));
 
-  displayImages = () => {
-    let {numImages} = this.state;
-    let images = [];
 
-    for(let i = 0; i < numImages; i++) {
-      images.push(
-        <img src={building} alt="building"/>
-      );
-    }
-
-    return (
-      images
-    );
   }
-
-  handleChange = (event) => {
-    this.setState({numImages: event.target.value});
-  }
+  
 
   render() {
     return (
       <div>
-        <div>Please choose number of images
-          <select value={this.state.numImages} onChange={this.handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
+        <div>Please select your card type.
+          <select onChange={creditordebit} id="cardselect">
+          <option value="">-</option>
+            <option value="credit">Credit</option>
+            <option value="debit">Debit</option>
           </select>
+          <table>
+            <thead>
+              <tr>
+                 <th>Date</th>
+                 <th>Transaction</th>
+                 <th>Amount</th>
+                 <th>ID</th>
+               </tr>
+             </thead>
+             <tbody id="myTable">
+             </tbody>
+            </table>
         </div>
-        {this.displayImages()}
       </div>
     );
   }
 }
+
 
 export default Content;
